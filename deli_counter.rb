703550be46c513fe3df2784b184d6503fe084064
Kevin  Rivera <krivera1015@gmail.com>
2018-07-12 02:44:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   array.each_with_index do |name, n|
     new_array << "The line is currently: #{n + 1}. #{name}"
   end
-  puts new_array.reduce
+  puts new_array.join
 else
   puts "The line is currently empty."
   end
