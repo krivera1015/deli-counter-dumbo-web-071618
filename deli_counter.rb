130ae431426e20e18030150e1699863c8c2a825e
Kevin  Rivera <krivera1015@gmail.com>
2018-07-12 02:44:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(array)
+  new_array = []
+  if array.length > 0
+  array.each_with_index do |name, n|
+    new_array << "The line is currently: #{n + 1}. #{name}"
+  end
+  puts new_array.reduce
+else
+  puts "The line is currently empty."
+  end
+end
