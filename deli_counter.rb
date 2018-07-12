2d0f2f5e5254705244d069d2bff382d298a65729
Kevin  Rivera <krivera1015@gmail.com>
2018-07-12 02:45:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(array)
-  new_array = []
+  new_array = ["The line is currently:"]
   if array.length > 0
   array.each_with_index do |name, n|
     new_array << "The line is currently: #{n + 1}. #{name}"
